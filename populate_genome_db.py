28094fc4abd4b8a10932398e919a9d68998ae971
shafferm <michael.shaffer@ucdenver.edu>
2016-11-16 21:23:21 -0700

    Working multiprocessed populat_genome_db.py ready to run on the server to fully populate the db

--- populate_genome_db.py
+++ populate_genome_db.py
@@ -1,3 +1,4 @@
+import argparse
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, Genome
@@ -14,50 +15,55 @@ session = DBSession()
 
 
 GG_LOC = "/Users/shafferm/lab/HIV_5runs/qiime_files/99_otu_taxonomy.txt"
-chunk_size = 2000
+chunk_size = 10
 procs = 3
 
 
-def chunks(l, n):
+def breakup_list(l, n):
     """Yield successive n-sized chunks from l."""
     for i in range(0, len(l), n):
         yield l[i:i + n]
 
 
-def generate_genome(chunk):
-    if len(chunk) == 1:
-        genome_table = load_data_table(list(chunk[0][0]))
-    else:
-        genome_table = load_data_table([i[0] for i in chunk])
-    for otu in chunk:
-        otu_id = otu[0]
-        taxonomy = otu[1]
+def generate_genome(otus):
+    genome_table = load_data_table([i[0] for i in otus])
+    genomes = list()
+    for otu_id, taxonomy in otus:
         nsti = genome_table.metadata(otu_id)['NSTI']
         genome = genome_table.ids(axis="observation")[genome_table.data(otu_id) > 0]
         genome = [str(i) for i in genome]
-        metab_network = make_metabolic_network(genome, min_component_size=4)
+        metab_network = make_metabolic_network(genome)
         metab_network_json = cy.from_networkx(metab_network)
         genome = Genome(name=int(otu_id), nsti=float(nsti), metab_net=json.dumps(metab_network_json),
                         genome=str(genome), taxonomy=taxonomy)
-        session.add(genome)
-        session.commit()
-        print otu_id
+        genomes.append(genome)
+    return genomes
 
 
-def main():
-    gg_genomes = {i.strip().split('\t')[0]: i.strip().split('\t')[1] for i in open(GG_LOC).readlines()}
+def add_to_db(chunks):
+    for chunk in chunks:
+        for genome in chunk:
+            session.add(genome)
+            session.commit()
+            print genome.name
 
-    # pool = multiprocessing.Pool(procs)
+
+def main(args):
+    gg_genomes = {i.strip().split('\t')[0]: i.strip().split('\t')[1] for i in open(args.gg_loc).readlines()[:100]}
+
+    chunks = breakup_list(gg_genomes.items(), chunk_size)
+    pool = multiprocessing.Pool(args.nprocs)
+    pool.map_async(generate_genome, chunks, callback=add_to_db)
+    pool.close()
+    pool.join()
 
     # for chunk in chunks(gg_genomes.items(), chunk_size):
-    #     pool.map(generate_genome, chunk)
-    #     # potentially have return objects and add to database not concurrently with a loop here
-    #     pool.close()
-    #     pool.join()
-
-    for chunk in chunks(gg_genomes.items(), chunk_size):
-        generate_genome(chunk)
+    #     generate_genome(chunk)
 
 
 if __name__ == "__main__":
-    main()
+    parser = argparse.ArgumentParser(formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument("--gg_loc", help="greengenes taxonomy location", default=GG_LOC)
+    parser.add_argument("--nprocs", help="number of processors", default=procs)
+    args = parser.parse_args()
+    main(args)
